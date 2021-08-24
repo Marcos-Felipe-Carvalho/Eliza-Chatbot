d539af737d67cab2bf0ad687d6a5785599bd3667
Marcos Felipe Carvalho <marcosfelipec2001@gmail.com>
2021-08-23 21:00:21 -0300

    Implementado validações no formulário de cadastro

--- src/app/authentication/register/register.component.ts
+++ src/app/authentication/register/register.component.ts
@@ -1,4 +1,6 @@
+import { FormValidations } from './../../shared/form/form-validations';
 import { Component, OnInit } from '@angular/core';
+import { Form, FormGroup, FormControl,Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -7,7 +9,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
+  public formRegister:FormGroup = new FormGroup({
+    fullName: new FormControl('',[Validators.required]),
+    email: new FormControl('',[Validators.required, Validators.pattern("^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$")]),
+    password: new FormControl('',[Validators.required]),
+    confirmPassword: new FormControl('', [FormValidations.equalsTo('password'), Validators.required])
+  })
+
   constructor() { }
+
+  onSubmit(){
+    console.log(this.formRegister.get('confirmPassword').value)
+    console.log(this.formRegister.get('password').value)
+  }
 
   ngOnInit(): void {
   }
