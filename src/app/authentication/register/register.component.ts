ed1119c02fae4124469b7cca127b1173bfdaffeb
Marcos Felipe Carvalho <marcosfelipec2001@gmail.com>
2021-08-24 11:50:55 -0300

    Implementado tamanho mínimo no campo de senha

--- src/app/authentication/register/register.component.ts
+++ src/app/authentication/register/register.component.ts
@@ -12,15 +12,16 @@ export class RegisterComponent implements OnInit {
   public formRegister:FormGroup = new FormGroup({
     fullName: new FormControl('',[Validators.required]),
     email: new FormControl('',[Validators.required, Validators.pattern("^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$")]),
-    password: new FormControl('',[Validators.required]),
+    password: new FormControl('',[Validators.required, Validators.minLength(8), /*FormValidations.requiredMin('password',8)*/]),
     confirmPassword: new FormControl('', [FormValidations.equalsTo('password'), Validators.required])
   })
 
   constructor() { }
 
   onSubmit(){
-    console.log(this.formRegister.get('confirmPassword').value)
+    /*console.log(this.formRegister.get('confirmPassword').value)
     console.log(this.formRegister.get('password').value)
+    console.log(this.formRegister.get('password'))*/
   }
 
   ngOnInit(): void {
